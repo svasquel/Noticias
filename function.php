50e7ce91021dddb3a319b9aa954d72ed2c9867a8
svasquel <susanavasquez.elias@gmail.com>
2012-09-28 22:29:20 +0200

    Tercera versión

--- function.php
+++ function.php
@@ -1,55 +1,45 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="title" content="Fundació Esportiva L'Hospitalet Atlètic" />
-<meta name="keywords" content="FELHA, L'Hospitalet, Atlètic, Fundació, Esportiva, valors, equip, futbol, Llobregat, Javier Río Romero" />
+<title>Noticias</title>
+<link rel="stylesheet" type="text/css" href="base.css"/>
 
-<title>Fundació Esportiva L'Hospitalet Atlètic</title>
-<link rel="stylesheet" type="text/css" href="../../css/reset.css"/>
-<link rel="stylesheet" type="text/css" href="../../css/base.css"/>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js" type="text/javascript"></script>
-<script type="text/javascript" src="../../js/jquery.tipsy.js"></script>
-<style type="text/css">html{background:#f8f8f8}</style>
 </head>
-
 <body>
-	<div id="container">
-	<div id="content">
-	
 		<div class="box alerts">
 		<h2>Accions realitzades amb èxit</h2>
 <?
-//VARIABLES//
+//FILE NAME
+$file=date("U");
+$file= 9999999999-$file;
+
+//GENERAL VARS//
+$image = "pics/".$file.".jpg"; //GENERAL IMAGE NAME
+$docs = "docs/"; //DOCUMENTS FOLDER
+$news = "news/"; //DOCUMENTS FOLDER
+$news = "news-change/"; //CHANGE FOLDER
+
+
+
+//VARIABLES ESPECÍFICAS//
 if (isset($_POST['title'])){
 $author = stripslashes($_POST['author']);
 $title = stripslashes($_POST['title']);
 $content = stripslashes($_POST['content']);
-$main =$_POST['main'];
-$file=date("U");
-$file= 9999999999-$file;
 
 $link = stripslashes($_POST['link']);
-if ($link !==""){
-	if(strpos($link,'youtube')!==FALSE){
-	$link_content='
-	<div class="box" style="width:470px; height:300px; padding:10px">
-		'.$link.'
-	</div>';
-	$link_content_es=$link_content;
-	$fp=fopen("../../youtube/$file.txt","w+");
-	fwrite($fp,$link);
-	fclose($fp);
+	if ($link !==""){
+		if(strpos($link,'youtube')!==FALSE){
+		$link_content='
+		<div class="box" style="width:470px; height:300px; padding:10px">
+			'.$link.'
+		</div>';
 		}
-	else{
-		$link_content = '<p><a href="'.$link.'" target="_blank">Veure enllaç</a></p>';
-		}
-}
-else{$link_content = "";}
-if($main=="_main"){$change_main="selected"; $change_second=" ";}
-else{$change_main=" "; $change_second="selected";}
+		else{$link_content = '<p><a href="'.$link.'" target="_blank">Veure enllaç</a></p>';}
+	}
+	else{$link_content = "";}
 
-$href = "news/".$file."/index.php";
-$list_name = ','.$file.$main;
+$href = $file."/index.php";
 
 //DATE FORMAT//
 $date_num = date("d/m/y");
@@ -96,129 +86,156 @@ if($Mes=='Abril'||$Mes=='Agost'||$Mes=='Octubre'){
 
 //GENERATE  DIRECTORY//
 
-mkdir("$file", 0777,true);
+mkdir("$news$file", 0777,true);
 
 //GENERATE IMAGES & PDFs//
 ?>
 <p>Si has adjuntat imatge per a la notícia, aquest element se't mostrarà en verd:</p>
 <ul>
 <?
-$image = "/pics/".$file.".jpg";
 if(is_uploaded_file($_FILES['image']['tmp_name'])){
-	if(move_uploaded_file($_FILES['image']['tmp_name'], $image)) {echo '<li class="positive">La imatge s\'ha pujat correctament</li>'; $image_class = " "; $noimage = "style=\"display:none\" ";}
-	else	{	echo "<li class='negative'>Hi ha hagut un problema en la pujada de la imatge, torna-ho a intentar.</li>";}
-	}
-else {echo '<li class="negative">No has seleccionat cap imatge</li>'; $image_class = "style=\"display:none\" "; $noimage = " ";}
+	if(move_uploaded_file($_FILES['image']['tmp_name'], $image)) {echo '<li class="positive">La imatge s\'ha pujat correctament</li>'; $image_class = " ";}
+	else{echo "<li class='negative'>Hi ha hagut un problema en la pujada de la imatge, torna-ho a intentar.</li>";}
+}
+else {echo '<li class="negative">No has seleccionat cap imatge</li>'; $image_class = "style=\"display:none\" ";}
 ?>
 </ul>
 <p>Informació detallada dels arxius penjats:</p>
 <ul>
 
 <?php 
-   //Preguntamos si nuetro arreglo 'archivos' fue definido
-         if (isset ($_FILES["archivos"])) {
-         //de se asi, para procesar los archivos subidos al servidor solo debemos recorrerlo
-         //obtenemos la cantidad de elementos que tiene el arreglo archivos
-         $tot = count($_FILES["archivos"]["name"]);
-         //este for recorre el arreglo
-         for ($i = 0; $i < $tot; $i++){
-         //con el indice $i, poemos obtener la propiedad que desemos de cada archivo
-         //para trabajar con este
-            $tmp_name = $_FILES["archivos"]["tmp_name"][$i];
-            $name = $_FILES["archivos"]["name"][$i];  
-			$archivo_title = stripslashes($_POST['archivo_title'][$i]);
-			if($archivo_title ==NULL){$archivo_title =$_FILES["archivos"]["name"][$i];}
-			$destino = "/docs/".$file."_".$i.".pdf";
-			$check[$i]='';
-				if(is_uploaded_file($_FILES["archivos"]["tmp_name"][$i])){
-					if(move_uploaded_file($_FILES["archivos"]["tmp_name"][$i],$destino))
-						{echo "<li class='positive'>S'ha penjat correctament <i>".$archivo_title."</i></li>"; $display[$i] = " "; $create = $destino; $hr[$i]="yes";}
-					else{
-						echo "<li class='negative'>Hi ha hagut un problema en la pujada de <i>".$archivo_title."</i>, torna-ho a intentar.</li>";}
+if (isset ($_FILES["archivos"])) {
+	$tot = count($_FILES["archivos"]["name"]);
+	for ($i = 0; $i < $tot; $i++){
+		$pdfs="";
+        $tmp_name = $_FILES["archivos"]["tmp_name"][$i];
+        $name = $_FILES["archivos"]["name"][$i];
+		$ext = explode(".", $_FILES["archivos"]["name"][$i]);
+		$archivo_title = stripslashes($_POST['archivo_title'][$i]);
+		if($archivo_title ==NULL){$archivo_title =$_FILES["archivos"]["name"][$i];}
+		$destino = $docs.$file."_".$i.".".$ext[count($ext)-1];
+		$check[$i]='';
+		if(is_uploaded_file($_FILES["archivos"]["tmp_name"][$i])){
+			if(move_uploaded_file($_FILES["archivos"]["tmp_name"][$i],$destino)){
+				echo "<li class='positive'>S'ha penjat correctament <i>".$archivo_title."</i> ";
+				$create = $destino;
+				$fp=fopen($docs.$file."_".$i.".txt","w+");
+				if(fwrite($fp,$archivo_title)){echo  "i a més s'ha penjat el seu títol </li>";}
+				else{echo  "però no s'ha penjat el seu títol </li>";}
+				fclose($fp);
+			}
+			else{
+				echo "<li class='negative'>Hi ha hagut un problema en la pujada de <i>".$archivo_title."</i>, torna-ho a intentar.</li>";
+			}
 		}
-else {	echo "<li class='negative'>No s'ha seleccionat cap fitxer per al <i>PDF número ".$i."</i></li>"; $display[$i] = "style=\"display:none\" "; $hr[$i]="no";}
-}
+		else{
+			echo "<li class='negative'>No s'ha seleccionat cap fitxer per al <i>PDF número ".$i."</i></li>"; $display[$i] = "style=\"display:none\" ";
+		}
+	}
 }      
 ?>
-
-
-
-
 
 </ul>
 <?
 //GENERATE INDEX.HTML//
+$index="
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+<title>Noticias -<? include 'title.html' ;?> </title>
+</head>
+<body>
+
+<div>
+	<h2><? include 'title.html' ;?></h2>
+	<hr>
+
+	<div>
+		<span class='author'><? include 'author.html' ;?></span>
+		<span class='date'><? include 'date.html' ;?></span>
+	</div>
+
+	<div <? include 'image_class.html' ;?>>
+		<span style='background:url(../../".$image.") no-repeat 0 top; background-size:cover'></span>
+	</div>
+
+	<p><? include 'content.html' ;?></p>
+
+	<ul>
+	<? 
+	\$file= glob('../../".$docs.$file."_*');
+	for (\$i = 0; \$i < count(\$file); \$i++) {
+		if(end(explode('.', \$file[\$i]))!=='txt'){
+			\$exp = explode('.',\$file[\$i]);
+			\$title_name = '../..'.\$exp[count(\$exp)-2].'.txt';
+			\$title_file= fopen(\$title_name,'r');
+			\$title= fread(\$title_file,filesize(\$title_name));
+			\$title = utf8_encode(\$title);
+			echo '<li><a href=\"'.\$file[\$i].'\" target=\"_blank\">'.\$title.'</a></li>';
+		}
+	}
+	?>
+	</ul>
+
+</div>
+
+</body>
+</html>
+";
+$change="
+
+";
 ?>
 <p>Més informació (aquests elements sempre han de mostrar-se en verd):</p>
 		<ul>
 <?
 
 //GENERATE CHANGE FILE//
-		$fp=fopen("$file.php","w+");
+		$fp=fopen("$change$file.php","w+");
 		if(fwrite($fp,$change)){echo "<li class='positive'>S'ha penjat el formulari editable de la notícia</li>";}
 			else{echo "<li class='negative'> NO s'ha penjat el formulari editable de la notícia</li>";}
 		fclose($fp);
 
 //GENERATE FILES CATALA//
-		$fp=fopen("../../src/ca/news/$file/title.html","w+");
-		if(fwrite($fp,$title)){echo  "<li class='positive'>S'ha penjat el títol (versió català)</li>";}
-			else{echo  "<li class='negative'>NO s'ha penjat el títol (versió català)</li>";}
+		$fp=fopen("$news$file/title.html","w+");
+		if(fwrite($fp,$title)){echo  "<li class='positive'>S'ha penjat el títol</li>";}
+			else{echo  "<li class='negative'>NO s'ha penjat el títol</li>";}
 		fclose($fp);
-		$fp=fopen("../../src/ca/news/$file/date.html","w+");
-		if(fwrite($fp,$date)){echo  "<li class='positive'>S'ha penjat la data (versió català)</li>";}
-			else{echo  "<li class='negative'>NO s'ha penjat la data (versió català)</li>";}
+		$fp=fopen("$news$file/author.html","w+");
+		if(fwrite($fp,$author)){echo  "<li class='positive'>S'ha penjat l'autor</li>";}
+			else{echo  "<li class='negative'>NO s'ha penjat l'autor</li>";}
 		fclose($fp);
-		$fp=fopen("../../src/ca/news/$file/date_num.html","w+");
-		if(fwrite($fp,$date_num)){echo  "<li class='positive'>S'ha penjat la data numèrica (versió català)</li>";}
-			else{echo  "<li class='negative'>NO s'ha penjat la data numèrica (versió català)</li>";}
+		$fp=fopen("$news$file/date.html","w+");
+		if(fwrite($fp,$date)){echo  "<li class='positive'>S'ha penjat la data</li>";}
+			else{echo  "<li class='negative'>NO s'ha penjat la data</li>";}
 		fclose($fp);
-		$fp=fopen("../../src/ca/news/$file/content.html","w+");
-		if(fwrite($fp,$content)){echo  "<li class='positive'>S'ha penjat el cos (versió català)</li>";}
-			else{echo  "<li class='negative'>NO s'ha penjat el cos (versió català)</li>";}
+		$fp=fopen("$news$file/date_num.html","w+");
+		if(fwrite($fp,$date_num)){echo  "<li class='positive'>S'ha penjat la data numèrica</li>";}
+			else{echo  "<li class='negative'>NO s'ha penjat la data numèrica</li>";}
 		fclose($fp);
-		$fp=fopen("../../src/ca/news/list.txt","a+");
-		fwrite($fp,$list_name);	
+		$fp=fopen("$news$file/content.html","w+");
+		if(fwrite($fp,$content)){echo  "<li class='positive'>S'ha penjat el cos</li>";}
+			else{echo  "<li class='negative'>NO s'ha penjat el cos</li>";}
 		fclose($fp);
-		$fp=fopen("../../src/ca/news/$file/href.html","w+");
+		$fp=fopen("list.txt","a+");
+		fwrite($fp,','.$file);	
+		fclose($fp);
+		$fp=fopen("$news$file/href.html","w+");
 		fwrite($fp,$href);	
 		fclose($fp);
-		$fp=fopen("../../src/ca/news/$file/index.php","w+");
-		if(fwrite($fp,$index)){echo  "<li class='positive'>S'ha penjat la notícia (versió català)</li>";}
-			else{echo  "<li class='negative'>NO s'ha penjat la notícia (versió català)</li>";}
+		$fp=fopen("$news$file/image_class.html","w+");
+		fwrite($fp,$image_class);	
 		fclose($fp);
-		
-//GENERATE FILES CASTELLA//
-		$fp=fopen("../../src/es/news/$file/title.html","w+");
-		if(fwrite($fp,$title_es)){echo  "<li class='positive'>S'ha penjat el títol (versió castellà)</li>";}
-			else{echo  "<li class='negative'>NO s'ha penjat el títol (versió castellà)</li>";}
-		fclose($fp);
-		$fp=fopen("../../src/es/news/$file/date.html","w+");
-		if(fwrite($fp,$date_es)){echo  "<li class='positive'>S'ha penjat la data (versió castellà)</li>";}
-			else{echo  "<li class='negative'>NO s'ha penjat la data (versió castellà)</li>";}
-		fclose($fp);
-		$fp=fopen("../../src/es/news/$file/date_num.html","w+");
-		if(fwrite($fp,$date_num)){echo  "<li class='positive'>S'ha penjat la data numèrica (versió castellà)</li>";}
-			else{echo  "<li class='negative'>NO s'ha penjat la data numèrica (versió castellà)</li>";}
-		fclose($fp);
-		$fp=fopen("../../src/es/news/$file/content.html","w+");
-		if(fwrite($fp,$content_es)){echo  "<li class='positive'>S'ha penjat el cos (versió castellà)</li>";}
-			else{echo  "<li class='negative'>NO s'ha penjat el cos (versió castellà)</li>";}
-		fclose($fp);
-		$fp=fopen("../../src/es/news/list.txt","a+");
-		fwrite($fp,$list_name);	
-		fclose($fp);
-		$fp=fopen("../../src/es/news/$file/href.html","w+");
-		fwrite($fp,$href);	
-		fclose($fp);
-		$fp=fopen("../../src/es/news/$file/index.php","w+");
-		if(fwrite($fp,$index_es)){echo  "<li class='positive'>S'ha penjat la notícia (versió castellà)</li>";}
-			else{echo  "<li class='negative'>NO s'ha penjat la notícia (versió castellà)</li>";}
+		$fp=fopen("$news$file/index.php","w+");
+		if(fwrite($fp,$index)){echo  "<li class='positive'>S'ha penjat la notícia</li>";}
+			else{echo  "<li class='negative'>NO s'ha penjat la notícia</li>";}
 		fclose($fp);
 }
 ?>
         
 		</ul>
-        <a class="gestio-new" href="news-form.php">Enrere</a>
+        <a class="gestio-new" href="form.php">Enrere</a>
 		</div><!--end .box-->
 	
 	</div><!--end #content-->
